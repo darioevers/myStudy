ab97e4a191a0b169e8d2422fc1bf29d3316b58e7
Dario Evers <darioevers@gmail.com>
2021-01-14 09:38:59 +0100

    update

---
+++ Assignments/January21/14-01/index.js
@@ -0,0 +1,33 @@
+/* let is a preserved keyword from javascript to declare a value.
+console is a object
+log is a method
+typeof is a function
+*/
+
+// TYPEOF
+
+console.log
+
+// BOOLEANS
+
+let bool = true;
+console.log(bool);
+let check = 12<20;
+console.log(1 == 100);
+let str1= 'Hi';
+let str2 = ' I am Dario';
+console.log('check =', check);
+console.log(str1 + str2);
+
+// CONDITIONS
+// =
+// === value and type
+// !== not equal / the opposite
+console.log(!bool);
+let bool2 = false;
+console.log(!bool2);
+console.log(1 !== 10);
+// >=
+// <==
+// >
+// <
