ee9cd9d4f4b8622e4361838046683e0685a17a85
Dario Evers <darioevers@gmail.com>
2021-01-14 09:47:09 +0100

    update

--- Assignments/January21/14-01/index.js
+++ Assignments/January21/14-01/index.js
@@ -5,11 +5,10 @@ typeof is a function
 */
 
 // TYPEOF
-
 console.log
 
-// BOOLEANS
 
+// BOOLEANS
 let bool = true;
 console.log(bool);
 let check = 12<20;
@@ -19,9 +18,15 @@ let str2 = ' I am Dario';
 console.log('check =', check);
 console.log(str1 + str2);
 
+
 // CONDITIONS
 // =
+let age = 12;
+let age2 = '12';
+// ==
+console.log(' value check ==', age == age2); // true
 // === value and type
+console.log(' value and type check ==', age === age2); // false
 // !== not equal / the opposite
 console.log(!bool);
 let bool2 = false;
@@ -30,4 +35,9 @@ console.log(1 !== 10);
 // >=
 // <==
 // >
+let che = 1 > 12;
 // <
+let che2 = 1 < 12;
+
+
+// ternary operator
