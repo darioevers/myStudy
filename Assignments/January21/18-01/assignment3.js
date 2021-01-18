8aaba8fefe53190040c57c8538ef97c4277288c3
Dario Evers <darioevers@gmail.com>
2021-01-18 13:56:01 +0100

    Create assignment3.js

---
+++ Assignments/January21/18-01/assignment3.js
@@ -0,0 +1,14 @@
+let color = "red";
+switch (color) {
+  case "blue":
+    console.log("This is Blue.");
+    break;
+  case "red":
+    console.log("This is Red.");
+    break;
+  case "yellow":
+    console.log("This is Yellow.");
+    break;
+  default:
+    console.log("This is the wrong color.");
+}
