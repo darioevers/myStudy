c840a07812722d44cac6f43ae00c844164f5b01a
Dario Evers <darioevers@gmail.com>
2021-01-18 09:17:31 +0100

    update

--- Assignments/January21/18-01/assignment.js
+++ Assignments/January21/18-01/assignment.js
@@ -1,5 +1,12 @@
 // for loop
 
-for (let i = 0; i < 5; i++) {
-  console.log("Hi");
+for (let i = 0; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("---------");
+
+for (let i = 0; i <= 8; ) {
+  console.log(i);
+  i += 2;
 }
