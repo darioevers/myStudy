5f99042c1afe3d71aa2da2a265c09f272a59e02b
Dario Evers <darioevers@gmail.com>
2021-01-18 13:50:31 +0100

    Update assignment2.js

--- Assignments/January21/18-01/assignment2.js
+++ Assignments/January21/18-01/assignment2.js
@@ -7,18 +7,13 @@ Print a string to the console containing the variable from step 3 using string i
 Create an if statement which prints the name and BMI of the person with the highest BMI.
 */
 
+// VARIABLES STORING THE HEIGHT AND WEIGHT
 var markHeight = 1.65;
 var markWeight = 72.5;
 var johnHeight = 1.88;
 var johnWeight = 84.5;
 var johnIndex = johnWeight / johnHeight ** 2;
-console.log(
-  `John has a Body-Mass-Index of ${johnIndex}, which is considered normal.`
-);
 var markIndex = markWeight / markHeight ** 2;
-console.log(
-  `Mark has a Body-Mass-Index of ${markIndex}, which is considered overweight.`
-);
 if (markIndex < johnIndex) {
   console.log(`John has a higher BMI by ${markIndex - johnIndex} BMI Points.`);
 } else {
@@ -26,7 +21,6 @@ if (markIndex < johnIndex) {
 }
 
 let bool = markIndex > johnIndex;
-console.log(bool);
 if ((bool = false)) {
   console.log(`John has a higher BMI by ${markIndex - johnIndex} BMI Points.`);
 } else {
@@ -39,9 +33,23 @@ Create an if/else statement to check whether the person’s age is less than 13.
 If the age is equal to or more than 13 and less than 20, print “firstName is a teenager”. 
 If the person’s age is equal to and more than 20 and less than 30, then print “firstName is a young adult”. 
 If none of these conditions apply, print “firstName is a adult”.
-
-
-var firstName = 'Dario';
-var age = 25;
-if (age <=)
 */
+
+let firstName = "Dario";
+let age = 25;
+const lifeStatus = (firstName, age) => {
+  let status = "";
+  if (age < 13) {
+    status = "Child";
+  } else if ((age) => 13 && age < 20) {
+    status = "Teenager";
+  } else if ((age) => 20 && age < 30) {
+    status = "Young adult";
+  } else {
+    status = "Adult";
+  }
+  return `${firstName} is a ${status}.`;
+};
+
+console.log(lifeStatus(firstName, age));
+console.log(lifeStatus);
