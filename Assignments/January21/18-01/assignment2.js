e4ac3305db00a5106ba91ef50ac796403b4c4a8a
Dario Evers <darioevers@gmail.com>
2021-01-18 11:20:50 +0100

    Update assignment2.js

--- Assignments/January21/18-01/assignment2.js
+++ Assignments/January21/18-01/assignment2.js
@@ -25,14 +25,23 @@ if (markIndex < johnIndex) {
   console.log(`Mark has a higher BMI by ${markIndex - johnIndex} BMI Points.`);
 }
 
+let bool = markIndex > johnIndex;
+console.log(bool);
+if ((bool = false)) {
+  console.log(`John has a higher BMI by ${markIndex - johnIndex} BMI Points.`);
+} else {
+  console.log(`Mark has a higher BMI by ${markIndex - johnIndex} BMI Points.`);
+}
+
 /* 
 Make a variable for firstName and age and give each variable values. 
 Create an if/else statement to check whether the person’s age is less than 13. If so, print “firstName is a child”. 
 If the age is equal to or more than 13 and less than 20, print “firstName is a teenager”. 
 If the person’s age is equal to and more than 20 and less than 30, then print “firstName is a young adult”. 
 If none of these conditions apply, print “firstName is a adult”.
-*/
+
 
 var firstName = 'Dario';
 var age = 25;
-if ()
+if (age <=)
+*/
