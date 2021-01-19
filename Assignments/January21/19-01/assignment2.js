00da17cf1e60b0a211cf33695f067d24c0ac4d2a
Dario Evers <darioevers@gmail.com>
2021-01-19 15:33:01 +0100

    Update assignment2.js

--- Assignments/January21/19-01/assignment2.js
+++ Assignments/January21/19-01/assignment2.js
@@ -54,10 +54,34 @@ i.e. findVowels(“this is a string”) ➞ 4
 */
 
 const findVowels = (str) => {
-  let findVowelsNr = str.split(["a", "e", "i", "o", "u"]).length - 1;
-  return findVowelsNr;
+  const vowels = ["a", "e", "i", "o", "u"];
+  const findVowelsNumber = str.split(vowels).length - 1;
+  return findVowelsNumber;
 };
 console.log(findVowels("Hello, how many vowels are here?"));
+
+const findVowels2 = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  var strBreakDown = str.split(vowels).length - 1;
+  var allVowels = vowels.includes(strBreakDown);
+  return allVowels;
+};
+console.log(findVowels2("Hello, this is my vowelcheck."));
+
+const findVowels3 = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  return str.split(vowels).length - 1;
+};
+console.log(findVowels3("Hello, how many vowels are here?"));
+
+const findVowels4 = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const strBreakDown = str.split("");
+  console.log(strBreakDown);
+  //const vowelCount = strBreakDown.split(vowels);
+  //return vowelCount;
+};
+console.log(findVowels4("Hello, this is my vowelcheck."));
 
 /*
 No Duplicates!
@@ -66,4 +90,16 @@ No Duplicates!
 [1, 6, 6, 9, 9] ➞ [1, 6, 9]
 [2, 2, 2, 2, 2, 2] ➞ [2]
 [5, 10, 15, 20, 25] ➞ [5, 10, 15, 20, 25]
+*/
+
+/*
+Dictionary. (take your time on this pls ) also as tip you can use .includes()
+ Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
+Notes:
+If none of the words match, return an empty array.
+Keep the filtered array in the same relative order as the original array of words.
+Examples:
+dictionary(“bu”, [“button”, “breakfast”, “border”]) ➞ [“button”]
+dictionary(“tri”, [“triplet”, “tries”, “trip”, “piano”, “tree”]) ➞ [“triplet”, “tries”, trip”]
+dictionary(“beau”, [“pastry”, “delicious”, “name”, “boring”]) ➞ []
 */
