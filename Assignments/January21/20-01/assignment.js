8f322cb65d638b3dac80683f2619795276ac3828
Dario Evers <darioevers@gmail.com>
2021-01-20 09:13:18 +0100

    Create assignment.js

---
+++ Assignments/January21/20-01/assignment.js
@@ -0,0 +1,15 @@
+const arr1 = [];
+arr1.push("HI");
+console.log(arr1);
+const arr2 = ["Good", "Day"];
+
+// new array
+const arr3 = new Array();
+arr3.push("cool");
+console.log(arr3);
+
+// .of
+
+// .sort
+// to sort elements in an array
+// it will use the UTF-16 CODE
