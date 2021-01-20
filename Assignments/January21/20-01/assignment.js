507ed480dc1b6776f70946b5b1a5810e9df2023e
Dario Evers <darioevers@gmail.com>
2021-01-20 09:20:51 +0100

    Update assignment.js

--- Assignments/January21/20-01/assignment.js
+++ Assignments/January21/20-01/assignment.js
@@ -1,3 +1,4 @@
+// The first way
 const arr1 = [];
 arr1.push("HI");
 console.log(arr1);
@@ -9,7 +10,12 @@ arr3.push("cool");
 console.log(arr3);
 
 // .of
+let newArr = Array.of(1, 20, 30, 40);
+console.log(newArr);
 
 // .sort
 // to sort elements in an array
 // it will use the UTF-16 CODE
+let alpha = ["c", "b", "a"];
+let newSortedAlpha = alpha.sort();
+console.log(newSortedAlpha);
