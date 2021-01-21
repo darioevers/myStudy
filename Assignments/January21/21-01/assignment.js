71d3f3988c2fc67ce0c73d93fcff2fe0471eb453
Dario Evers <darioevers@gmail.com>
2021-01-21 10:52:38 +0100

    Update assignment.js

--- Assignments/January21/21-01/assignment.js
+++ Assignments/January21/21-01/assignment.js
@@ -40,14 +40,48 @@ function halloUser() {
 // .map()
 //for (let i=0;i<Array.length;i++) <- .map is a shorter version of it.
 const names = ["Dario", "Elon Musk", ".map"];
-const cities = ["Hamburg", "Los Angeles"];
 const newArr = names.map((name, i) => {
   if ((name = ".map")) {
-    console.log("Blabla");
+    console.log(".map is not included");
   }
   console.log(`The name "${name}" has index nr. ${i}.`);
 });
 
+const cities = ["Hamburg", "Los Angeles"];
 const newCity = cities.map((city, i) =>
   console.log(`The city "${city}" has index nr. ${i}.`)
 );
+
+// .reduce()
+const numArr = [1, 2, 3, 34, 5, 56];
+const reducer = numArr.reduce((acc, cur) => acc + cur);
+console.log(reducer);
+
+// .break
+// breaking out of a loop:
+
+/*
+T
+TT
+TTT
+TTTT
+*/
+
+let TBox = "";
+for (let i = 0; i <= 3; i++) {
+  for (let j = 0; j <= 3; j++) {
+    if (i == j) {
+      break;
+    } else {
+      TBox += "T";
+      console.log(TBox);
+    }
+  }
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i == 4) {
+    break;
+  }
+}
