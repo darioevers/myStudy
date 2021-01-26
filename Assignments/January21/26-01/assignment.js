93bca3950635b1501c65594bb13d7b2da8387134
Dario Evers <darioevers@gmail.com>
2021-01-26 10:20:05 +0100

    update

--- Assignments/January21/26-01/assignment.js
+++ Assignments/January21/26-01/assignment.js
@@ -1,1 +1,33 @@
-//
+// ___________________________________________________________ASSIGNMENT 1___________________________________________________________:
+// Write a function that it returns  “Two for me and one for you” when no arguments are passed
+// e.g
+// console.log(twofer("Fran")) ---> "Two for me and one for Fran"
+// console.log(twofer()) ---> "Two for me and one for you"
+
+// ___________________________________________________________ASSIGNMENT 2___________________________________________________________:
+// Write a function  that it still calculates the power of a number even if the exp argument is not passed to it. The default exp should be set to 2.
+// e.g
+// console.log(exponent(3, 3)) ---> 27
+//console.log(exponent(3)) ---> 9
+
+// ___________________________________________________________ASSIGNMENT 3___________________________________________________________:
+//Write a function that it returns the total amount of arguments passed to it.
+//e.g
+//console.log(howManyArgs()) ---> 0
+//console.log(howManyArgs(1, false, "hello")) ---> 3
+//console.log(howManyArgs("better")) ---> 1
+
+// ___________________________________________________________ASSIGNMENT 4___________________________________________________________:
+//Write a function which accepts any amount of numbers and return the sum of their addition
+//e.g
+//console.log(sum(1)) ---> 1
+//console.log(sum(1, 15)) ---> 16
+//console.log(sum(25, 25, 20)) ---> 70
+
+// ___________________________________________________________ASSIGNMENT 5___________________________________________________________:
+//Bonus: Write a function which accepts any amount of numbers and returns the average.
+//e.g
+//console.log(average(0)) ---> 0
+//console.log(average(1, 2)) ---> 1.5
+//console.log(average(1, 3, 6, 10)) ---> 5
+//console.log(average(12, 14, 16)) ---> 14
