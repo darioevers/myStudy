1fdffe4e58a065d06d661d8d2b9a34ce6ac5f123
Dario Evers <darioevers@gmail.com>
2021-01-26 11:06:25 +0100

    Update assignment.js

--- Assignments/January21/26-01/assignment.js
+++ Assignments/January21/26-01/assignment.js
@@ -4,24 +4,32 @@
 // console.log(twofer("Fran")) ---> "Two for me and one for Fran"
 // console.log(twofer()) ---> "Two for me and one for you"
 
-//const twofer = (str) => {
-//  let result = "";
-//  var n = str.includes("", 0);
-//  if (n) {
-//    console.log(`Two for me and one for ${str}.`);
-//  } else {
-//    console.log(`Two for me and one for you.`);
-//  }
-///  return result;
-//};
-//twofer("Dario");
-//twofer();
+const twoFor = (who = "you") => `Two for me and one for ${who}`;
+console.log(twoFor("Dario"));
+console.log(twoFor());
 
 // ___________________________________________________________ASSIGNMENT 2___________________________________________________________:
 // Write a function  that it still calculates the power of a number even if the exp argument is not passed to it. The default exp should be set to 2.
 // e.g
 // console.log(exponent(3, 3)) ---> 27
-//console.log(exponent(3)) ---> 9
+// console.log(exponent(3)) ---> 9
+const exponent = (num, exp = 2) => num ** exp;
+console.log(exponent(3, 3));
+console.log(exponent(3));
+
+const exponent2 = (num, exp = 2) => Math.pow(num, exp);
+console.log(exponent2(3, 3));
+console.log(exponent2(3));
+
+const exponent3 = (num, exp = 2) => {
+  let result = 1;
+  for (let i = 0; i < exp; i++) {
+    result *= num;
+  }
+  return result;
+};
+console.log(exponent3(3, 3));
+console.log(exponent3(3));
 
 // ___________________________________________________________ASSIGNMENT 3___________________________________________________________:
 //Write a function that it returns the total amount of arguments passed to it.
@@ -54,6 +62,18 @@ console.log(sum([25, 25, 20]));
 //console.log(average(1, 2)) ---> 1.5
 //console.log(average(1, 3, 6, 10)) ---> 5
 //console.log(average(12, 14, 16)) ---> 14
+/*
+const average = (num) => {
+  let arr = num.split("");
+  var totalSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    arr.Math.floor();
+  }
+  return `The sum of adding up the numbers ${arr} is ${totalSum} in total.`;
+};
+console.log(average(0));
+console.log(average(1, 2));
+console.log(average(1, 3, 6, 10));
 /*
 const average = (num) => {
   let numArr = num.split(", ");
