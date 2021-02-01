7047b30e8d1ea1699099c2572a550b9486eaa356
Dario Evers <darioevers@gmail.com>
2021-02-01 09:45:32 +0100

    updated

---
+++ Assignments/February 21/01-02/assignment.js
@@ -0,0 +1,29 @@
+// let, var, const | local vs global
+// global
+// {local}
+
+// functions inside functions
+function main(num1, num2) {
+  // outer function
+  function second(n) {
+    // inner function
+    return n * n;
+  }
+  return second(num1) + second(num2);
+}
+console.log(main(3, 4));
+
+// Nested Scope: Accessing outer global variables
+let nice = true;
+const outerFunction = (x, y) => {
+  let big = true;
+  console.log(big + " outerFunction");
+  const innerFunction = (num) => {
+    big = false;
+    nice = false;
+    console.log(big + " innerFunction");
+    return num + 10;
+  };
+  return innerFunction(x) + innerFunction(y);
+};
+console.log(outerFunction(3, 4));
