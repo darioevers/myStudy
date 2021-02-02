1441d63a27478d33505e39cf0c0b191f4d5a3425
Dario Evers <darioevers@gmail.com>
2021-02-02 09:25:35 +0100

    Create assignment.js

---
+++ Assignments/February 21/02-02/assignment.js
@@ -0,0 +1,29 @@
+// OBJECTS
+
+// old life with no objects:
+let firstName = "Dario";
+let lastName = "Evers";
+let userAge = 26;
+
+// OBJECTS
+// Object Literal
+// const <name> = { <key1>: <value1>, ...}
+// The whole thing is called property. key:value!
+
+const obj = {
+  1: "Dario",
+  2: "Kid",
+  3: "Cudi",
+};
+
+const objUser = {
+  name: "Ercan",
+  lastName: "Erdagi",
+  age: 24,
+  address: "Pinneberg",
+  activity: ["Work", "Eat", "Drink", "Repeat"],
+  young: true,
+};
+console.log(objUser.name);
+console.log(objUser.activity[2]);
+console.log(typeof console);
