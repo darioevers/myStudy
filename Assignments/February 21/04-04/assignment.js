e1a5c8605b147916620d484dcafe2377cf9f2a49
Dario Evers <darioevers@gmail.com>
2021-02-04 11:56:32 +0100

    Update assignment.js

--- Assignments/February 21/04-04/assignment.js
+++ Assignments/February 21/04-04/assignment.js
@@ -13,3 +13,96 @@ console.log("__________ASSIGNMENT 2__________");
 // Examples:
 // zodiac(“14-02-2002”)  -> Aquarius
 // zodiac(“10-06-1984")  -> Gemini
+
+function zodiac(birthday) {
+  let sign = "";
+  let birthdayArr = birthday.split("-");
+  switch (birthdayArr) {
+    case (birthdayArr[1] == "01" && birthdayArr[0] >= 20) ||
+      (birthdayArr[1] == "February" && birthdayArr[0] <= 18):
+      sign = "Aquarius";
+      break;
+    case (birthdayArr[1] == "02" && birthdayArr[0] >= 19) ||
+      (birthdayArr[1] == "03" && birthdayArr[0] <= 20):
+      sign = "Pisces";
+      break;
+    case (birthdayArr[1] == "03" && birthdayArr[0] >= 21) ||
+      (birthdayArr[1] == "04" && birthdayArr[0] <= 19):
+      sign = "Aries";
+      break;
+    case (birthdayArr[1] == "04" && birthdayArr[0] >= 20) ||
+      (birthdayArr[1] == "05" && birthdayArr[0] <= 20):
+      sign = "Taurus";
+      break;
+    case (birthdayArr[1] == "05" && birthdayArr[0] >= 21) ||
+      (birthdayArr[1] == "06" && birthdayArr[0] <= 20):
+      sign = "Gemini";
+      break;
+    case (birthdayArr[1] == "06" && birthdayArr[0] >= 21) ||
+      (birthdayArr[1] == "07" && birthdayArr[0] <= 22):
+      sign = "Cancer";
+      break;
+    case (birthdayArr[1] == "07" && birthdayArr[0] >= 23) ||
+      (birthdayArr[1] == "08" && birthdayArr[0] <= 22):
+      sign = "Leo";
+      break;
+    case (birthdayArr[1] == "08" && birthdayArr[0] >= 23) ||
+      (birthdayArr[1] == "09" && birthdayArr[0] <= 22):
+      sign = "Virgo";
+      break;
+    case (birthdayArr[1] == "09" && birthdayArr[0] >= 23) ||
+      (birthdayArr[1] == "10" && birthdayArr[0] <= 22):
+      sign = "Libra";
+      break;
+    case (birthdayArr[1] == "10" && birthdayArr[0] >= 23) ||
+      (birthdayArr[1] == "11" && birthdayArr[0] <= 21):
+      sign = "Scorpio";
+      break;
+    case (birthdayArr[1] == "11" && birthdayArr[0] >= 22) ||
+      (birthdayArr[1] == "12" && birthdayArr[0] <= 21):
+      sign = "Saggitarius";
+    case (birthdayArr[1] == "12" && birthdayArr[0] >= 22) ||
+      (birthdayArr[1] == "01" && birthdayArr[0] <= 19):
+      sign = "Capricorn";
+      break;
+  }
+  return sign;
+}
+zodiac("10-06-1984");
+zodiac("14-02-2002");
+
+// Aries
+// 21 03 – 20 04
+
+// Taurus
+// 20 04 – 21 05
+
+// Gemini
+// 21 05 – 21 06
+
+// Cancer
+// 21 06 – 23 07
+
+// Leo
+// 23 07 – 23 08
+
+// Virgo
+// 23 08 – 23 09
+
+// Libra
+// 23 09 – 23 10
+
+// Scorpio
+// 23 10 – 23 11
+
+// Sagaittarius
+// 23 11 – 22 12
+
+// Capricornus
+// 22 12 - 20 01
+
+// Aquarius
+// 20 01 - 19 02
+
+// Pisces
+// 19 02 - 21 03
