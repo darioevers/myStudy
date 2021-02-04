7e1e202d6ffd85c7eed56d4afdadc2f3664aa2b0
Dario Evers <darioevers@gmail.com>
2021-02-04 15:28:17 +0100

    Update assignment.js

--- Assignments/February 21/04-04/assignment.js
+++ Assignments/February 21/04-04/assignment.js
@@ -14,7 +14,7 @@ console.log("__________ASSIGNMENT 2__________");
 // zodiac(“14-02-2002”)  -> Aquarius
 // zodiac(“10-06-1984")  -> Gemini
 function zodiac(birthday) {
-  let sign = "";
+  var sign = "";
   let birthdayArr = parseInt(birthday.split("-"));
   switch (birthdayArr) {
     case (birthdayArr[1] == 01 && birthdayArr[0] >= 20) ||
@@ -70,73 +70,78 @@ function zodiac(birthday) {
   }
   return sign;
 }
-zodiac("10-06-1984");
-zodiac("14-02-2002");
+console.log(zodiac("10-06-1984"));
+console.log(zodiac("14-02-2002"));
 
 // NEXT
+const zodiacDates = {
+  Aries: {
+    start: [21][3],
+    end: [20][4],
+  },
+  Taurus: {
+    start: [20][4],
+    end: [21][5],
+  },
+  Gemini: {
+    start: [21][5],
+    end: [21][6],
+  },
+  Cancer: {
+    start: [21][6],
+    end: [23][7],
+  },
+  Leo: {
+    start: [23][7],
+    end: [23][8],
+  },
+  Virgo: {
+    start: [23][8],
+    end: [23][9],
+  },
+  Libra: {
+    start: [23][9],
+    end: [23][10],
+  },
+  Scorpio: {
+    start: [23][10],
+    end: [22][11],
+  },
+  Sagittarius: {
+    start: [23][11],
+    end: [22][12],
+  },
+  Capricorn: {
+    start: [22][12],
+    end: [20][1],
+  },
+  Aquarius: {
+    start: [20][1],
+    end: [19][2],
+  },
+  Pisces: {
+    start: [19][2],
+    end: [21][3],
+  },
+};
 
-const zodiac2 = [
-  {
-    name: "Aries",
-    start: "21-03",
-    end: "20-04",
-  },
-  {
-    name: "Taurus",
-    start: "20-04",
-    end: "21-05",
-  },
-  {
-    name: "Gemini",
-    start: "21-05",
-    end: "21-06",
-  },
-  {
-    name: "Cancer",
-    start: "21-06",
-    end: "23-07",
-  },
-  {
-    name: "Leo",
-    start: "23-07",
-    end: "23-08",
-  },
-  {
-    name: "Virgo",
-    start: "23-08",
-    end: "23-09",
-  },
-  {
-    name: "Libra",
-    start: "23-09",
-    end: "23-10",
-  },
-  {
-    name: "Scorpio",
-    start: "23-10",
-    end: "22-11",
-  },
-  {
-    name: "Sagittarius",
-    start: "23-11",
-    end: "22-12",
-  },
-  {
-    name: "Capricorn",
-    start: "22-12",
-    end: "20-01",
-  },
-  {
-    name: "Aquarius",
-    start: "20-01",
-    end: "19-02",
-  },
-  {
-    name: "Pisces",
-    start: "19-02",
-    end: "21-03",
-  },
-];
+console.log("__________ASSIGNMENT__________");
+
+const zodiacSecond = (birthday) => {
+  sign = "";
+  let birthdayArr = birthday.split("-");
+  let day = parseInt(birthdayArr[0]);
+  let month = parseInt(birthdayArr[1]);
+  return Object.hasOwnProperty();
+  for (let prop in Object) {
+    date >= Object.id.start && date <= Object.id.end
+      ? Object.id
+      : `Please enter a valid date!`;
+  }
+};
+console.log(zodiacSecond("14-02-2002"));
+
+// NEXT
 
 // Aries
 // 21 03 – 20 04
