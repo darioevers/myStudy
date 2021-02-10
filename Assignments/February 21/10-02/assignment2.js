f8c7186a1e4fe217df8082d6d119ac4a336bb660
Dario Evers <darioevers@gmail.com>
2021-02-10 11:43:03 +0100

    Update assignment2.js

--- Assignments/February 21/10-02/assignment2.js
+++ Assignments/February 21/10-02/assignment2.js
@@ -1,3 +1,5 @@
+// ___________________________________________________________ASSIGNMENT 1___________________________________________________________:
+console.log("__________ASSIGNMENT 1__________");
 // Scrabble. Write a program that, given an array of scrabble tiles, counts the maximum score that a player can earn from the tiles in their hand. Example:
 //  [ { tile: “N”, score: 1 },
 //    { tile: “K”, score: 5 },
@@ -15,11 +17,35 @@ const scrabbleScores = [
   { tile: "A", score: 1 },
   { tile: "E", score: 1 },
 ];
-function printScores(arr) {
-  let counter = 0;
-  for (let i = 0; i < arr.length; i++) {
-    counter += arr[i]["score"];
-  }
-  return counter;
+const scrabble = (arr) => {
+  return arr.reduce((acc, cur) => acc + cur.score, 0);
+};
+console.log(scrabble(scrabbleScores));
+
+// ___________________________________________________________ASSIGNMENT 2___________________________________________________________:
+console.log("__________ASSIGNMENT 2__________");
+// Switch Keys and Values.
+// Create a function to get a copy of an object. The copy must switch the keys and values.
+// Example:
+// let person = {
+//   name: “John”,
+//   job: “teacher”
+// }
+// Expected Output:
+// {“John”: name, “teacher”: job}
+
+let person = {
+  name: "John",
+  job: "teacher",
+};
+function reverse() {
+    let revArr = 
 }
-console.log(printScores(scrabbleScores));
+
+// ___________________________________________________________ASSIGNMENT 3___________________________________________________________:
+console.log("__________ASSIGNMENT 3__________");
+// Return Keys and Values.
+// Write a program that takes an object and returns an array which contains two arrays: one for the keys of the object and the other for the values of the object.
+// Examples:
+// { a: 1, b: 2, c: 3 } ➞ [[“a”, “b”, “c”], [1, 2, 3]]
+// {key: true} ➞ [[“key”], [true]]
