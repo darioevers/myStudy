7a105ba2adc7748828a0a245cff96a59096d5877
Dario Evers <darioevers@gmail.com>
2021-02-11 13:45:55 +0100

    y

---
+++ February 21/11-01/TV.js
@@ -0,0 +1,14 @@
+// TV app
+class Tv {
+  constructor(_brand, _channel = 1, _volume = 10) {
+    this.brand = _brand;
+    this.channel = _channel;
+    this.volume = _volume;
+  }
+  incVolume() {}
+  decVolume() {}
+  rest() {
+    // volume and channel
+  }
+}
+// extends && super
