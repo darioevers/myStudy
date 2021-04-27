0492b256bead68c6f2e0635c349dcb57746d9846
Dario Evers <darioevers@gmail.com>
2021-04-27 09:24:34 +0200

    Update index.js

--- April 21/27-04/scripts/index.js
+++ April 21/27-04/scripts/index.js
@@ -1,8 +1,24 @@
 // Change the font of the body element.
-
+const body = document.querySelector("body");
+body.style.fontFamily = "Arial";
 // Center the text of h1 on the page.
+const h1 = document.querySelector("h1");
+h1.style.textAlign = "center";
+
+// main
+const mainSec = document.querySelector(".main");
+mainSec.style.display = "flex";
+mainSec.style.flexFlow = "row wrap";
+mainSec.style.justifyContent = "space-around";
+mainSec.style.boxSizing = "border-box";
 
 //The menu headings have a class named category. Select the elements by class name. Then, change the color and font-style of each element.
+const category = document.querySelectorAll(".category");
+category.forEach(({ style: cat }) => {
+  cat.fontSize = "1.5rem";
+  cat.fontStyle = "italic";
+  cat.borderBottom = "1px solid black";
+});
 
 //Create a function named "colorGenerator" that returns a different color. The returned random color should also include set transparency. Select the unordered lists with the class of food-category. Give each list a different background color using the function that you created.
 const colorGen = () => {
