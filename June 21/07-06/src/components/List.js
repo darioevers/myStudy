f5d9092745639e59f14054a07cf10dbc64d9a008
Dario Evers <darioevers@gmail.com>
2021-06-09 10:46:52 +0200

    a

---
+++ June 21/07-06/src/components/List.js
@@ -0,0 +1,10 @@
+
+const List = ({ setCount }) => {
+    return (
+      <div>
+        <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
+      </div>
+    );
+  };
+  
+  export default List;
