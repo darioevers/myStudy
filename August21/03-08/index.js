63215c24df54f244b455d97dd3b47ccc21497071
Dario Evers <darioevers@gmail.com>
2021-08-03 09:10:09 +0200

    added content part 1

--- August21/03-08/index.js
+++ August21/03-08/index.js
@@ -1,1 +1,21 @@
 const fs = require("fs");
+
+// create file
+// appendFile will create file
+fs.appendFile("newText.txt","This is text for newText.txt",(err)=>{
+    if (err) throw err;
+    console.log("File is created")
+})
+
+// read file
+
+// read file sync
+
+// rename file
+
+// delete file
+
+// copy file
+
+// open file
+// fs flags
